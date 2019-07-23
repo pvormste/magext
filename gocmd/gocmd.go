b01bddb186a6c8a6d2e046cc2f08c65c5cc634d4
Patric Vormstein <pvormstein@googlemail.com>
2019-07-23 20:28:59 +0200

    add godocs and install function

--- gocmd/gocmd.go
+++ gocmd/gocmd.go
@@ -13,6 +13,55 @@ var goInstall = sh.RunCmd(mg.GoCmd(), "install")
 
 var errModuleNotSpecified = errors.New("module was not specified")
 
+// Get represents the command for `go get`
+//
+// Example:
+//     gocmd.Get("github.com/pvormste/magext")
+//
+func Get(module string) error {
+	return execGetCmd(false, module, "")
+}
+
+// GetByVersion represents the command fot `go get` with specified version
+//
+// Example:
+//     gocmd.GetByVersion("github.com/pvormste/magext", "v1.0.0")
+//
+func GetByVersion(module, version string) error {
+	return execGetCmd(false, module, version)
+}
+
+// GetU represents the command for `go get -u`
+//
+// Example:
+//     gocmd.GetU("github.com/pvormste/magext")
+//
+func GetU(module string) error {
+	return execGetCmd(true, module, "")
+}
+
+// GetUByVersion represents the command for `go get -u` with specified version
+//
+// Example:
+//     gocmd.GetUByVersion("github.com/pvormste/magext", "v1.0.0")
+//
+func GetUByVersion(module, version string) error {
+	return execGetCmd(true, module, version)
+}
+
+// Install represents the command for `go install`
+//
+// Example:
+//     gocmd.Install("github.com/pvormste/magext")
+//
+func Install(module string) error {
+	if len(module) == 0 {
+		return errModuleNotSpecified
+	}
+
+	return goInstall(module)
+}
+
 func execGetCmd(withFlagU bool, module string, version string) error {
 	moduleParameter, err := createModuleParameter(module, version)
 	if err != nil {
@@ -24,22 +73,6 @@ func execGetCmd(withFlagU bool, module string, version string) error {
 	}
 
 	return goGet(moduleParameter)
-}
-
-func Get(module string) error {
-	return execGetCmd(false, module, "")
-}
-
-func GetByVersion(module, version string) error {
-	return execGetCmd(false, module, version)
-}
-
-func GetU(module string) error {
-	return execGetCmd(true, module, "")
-}
-
-func GetUByVersion(module, version string) error {
-	return execGetCmd(true, module, version)
 }
 
 func createModuleParameter(module, version string) (string, error) {

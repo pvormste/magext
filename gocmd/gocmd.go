29eb2b14e540e27810c041ce4162b40073ea7835
Patric Vormstein <pvormstein@googlemail.com>
2019-07-23 20:40:53 +0200

    add func variants with output

--- gocmd/gocmd.go
+++ gocmd/gocmd.go
@@ -9,14 +9,17 @@ import (
 )
 
 var goGet = sh.RunCmd(mg.GoCmd(), "get")
+var goGetWithOut = sh.OutCmd(mg.GoCmd(), "get")
+
 var goInstall = sh.RunCmd(mg.GoCmd(), "install")
+var goInstallWithOut = sh.OutCmd(mg.GoCmd(), "install")
 
 var errModuleNotSpecified = errors.New("module was not specified")
 
 // Get represents the command for `go get`
 //
 // Example:
-//     gocmd.Get("github.com/pvormste/magext")
+//     err := gocmd.Get("github.com/pvormste/magext")
 //
 func Get(module string) error {
 	return execGetCmd(false, module, "")
@@ -25,16 +28,35 @@ func Get(module string) error {
 // GetByVersion represents the command fot `go get` with specified version
 //
 // Example:
-//     gocmd.GetByVersion("github.com/pvormste/magext", "v1.0.0")
+//     err := gocmd.GetByVersion("github.com/pvormste/magext", "v1.0.0")
 //
 func GetByVersion(module, version string) error {
 	return execGetCmd(false, module, version)
 }
 
+// GetWithOutput represents the command for `go get` with the return of output
+//
+// Example:
+//     output, err := gocmd.GetWithOutput("github.com/pvormste/magext")
+//
+func GetWithOutput(module string) (string, error) {
+	return execGetCmdWithOut(false, module, "")
+}
+
+// GetByVersionWithOutput represents the command fot `go get` with specified version
+// and with return of output
+//
+// Example:
+//     output, err := gocmd.GetByVersionWithOutput("github.com/pvormste/magext", "v1.0.0")
+//
+func GetByVersionWithOutput(module, version string) (string, error) {
+	return execGetCmdWithOut(false, module, version)
+}
+
 // GetU represents the command for `go get -u`
 //
 // Example:
-//     gocmd.GetU("github.com/pvormste/magext")
+//     err := gocmd.GetU("github.com/pvormste/magext")
 //
 func GetU(module string) error {
 	return execGetCmd(true, module, "")
@@ -43,10 +65,29 @@ func GetU(module string) error {
 // GetUByVersion represents the command for `go get -u` with specified version
 //
 // Example:
-//     gocmd.GetUByVersion("github.com/pvormste/magext", "v1.0.0")
+//     err := gocmd.GetUByVersion("github.com/pvormste/magext", "v1.0.0")
 //
 func GetUByVersion(module, version string) error {
 	return execGetCmd(true, module, version)
+}
+
+// GetUWithOutput represents the command for `go get -u` with output
+//
+// Example:
+//     output, err := gocmd.GetUWithOutput("github.com/pvormste/magext")
+//
+func GetUWithOutput(module string) (string, error) {
+	return execGetCmdWithOut(true, module, "")
+}
+
+// GetUByVersionWithOutput represents the command for `go get -u` with specified version
+// and with output
+//
+// Example:
+//     output, err := gocmd.GetUByVersionWithOutput("github.com/pvormste/magext", "v1.0.0")
+//
+func GetUByVersionWithOutput(module, version string) (string, error) {
+	return execGetCmdWithOut(true, module, version)
 }
 
 // Install represents the command for `go install`
@@ -62,6 +103,19 @@ func Install(module string) error {
 	return goInstall(module)
 }
 
+// InstallWithOutput represents the command for `go install` with output
+//
+// Example:
+//     output, err := gocmd.InstallWithOutput("github.com/pvormste/magext")
+//
+func InstallWithOutput(module string) (string, error) {
+	if len(module) == 0 {
+		return "", errModuleNotSpecified
+	}
+
+	return goInstallWithOut(module)
+}
+
 func execGetCmd(withFlagU bool, module string, version string) error {
 	moduleParameter, err := createModuleParameter(module, version)
 	if err != nil {
@@ -73,6 +127,19 @@ func execGetCmd(withFlagU bool, module string, version string) error {
 	}
 
 	return goGet(moduleParameter)
+}
+
+func execGetCmdWithOut(withFlagU bool, module string, version string) (string, error) {
+	moduleParameter, err := createModuleParameter(module, version)
+	if err != nil {
+		return "", err
+	}
+
+	if withFlagU {
+		return goGetWithOut("-u", moduleParameter)
+	}
+
+	return goGetWithOut(moduleParameter)
 }
 
 func createModuleParameter(module, version string) (string, error) {
